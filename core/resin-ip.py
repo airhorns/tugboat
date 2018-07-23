427f454852496fff93635c5101bddefe120564ed
Harry Brundage <harry.brundage@gmail.com>
2018-07-23 14:36:19 -0400

    WIP

--- core/resin-ip.py
+++ core/resin-ip.py
@@ -2,8 +2,6 @@ import json
 import urllib2
 import os
 
-print os.environ
-
-data = json.load(urllib2.urlopen("{}/v1/device?api_key={}".format(os.environ['RESIN_SUPERVISOR_ADDRESS'], os.environ['RESIN_SUPERVISOR_API_KEY'])))
+data = json.load(urllib2.urlopen("{}/v1/device?apikey={}".format(os.environ['RESIN_SUPERVISOR_ADDRESS'], os.environ['RESIN_SUPERVISOR_API_KEY'])))
 
 print(data['ip_address'].split(' ')[0])
