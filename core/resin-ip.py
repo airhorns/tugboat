b4c34695b3f05fb993466a5a4847b32b83e486b9
Harry Brundage <harry.brundage@gmail.com>
2018-07-23 14:22:20 -0400

    Resin screwery

--- core/resin-ip.py
+++ core/resin-ip.py
@@ -2,6 +2,8 @@ import json
 import urllib2
 import os
 
+print os.environ
+
 data = json.load(urllib2.urlopen("{}/v1/device?api_key={}".format(os.environ['RESIN_SUPERVISOR_ADDRESS'], os.environ['RESIN_SUPERVISOR_API_KEY'])))
 
 print(data['ip_address'].split(' ')[0])
